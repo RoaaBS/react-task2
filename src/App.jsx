5556eae08c395305cef6c0aedfd83c4cc48f2bf4
RoaaBS <s11924302@stu.najah.edu>
2025-01-04 13:23:28 +0200

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+import "./index.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Welcome from "./Components/Welcome/welcome";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import About from "./Components/About/About";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './index.css';
-
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <>
       <Navbar />
       <Welcome />
-      <Routes>
-        <Route path='/' element={<Portfolio />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Contact' element={<Contact />} />
-      </Routes>
-      <Footer />
+      <Portfolio/>
+      <About/>
+      <Contact/>
+      <Footer/>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import ReactDOM from 'react-dom/client'; 
-import App from './App.jsx';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
-);
+  </StrictMode>,
+)
