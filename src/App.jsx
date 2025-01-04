e3ebff44dee26e38bb8de04fc7996025a1ca63a5
RoaaBS <s11924302@stu.najah.edu>
2025-01-04 13:18:59 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
-
-import "./index.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Welcome from "./Components/Welcome/welcome";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import About from "./Components/About/About";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
+
 function App() {
   return (
     <>
       <Navbar />
       <Welcome />
-      <Portfolio/>
-      <About/>
-      <Contact/>
-      <Footer/>
+      <Routes>
+        <Route path='/' element={<Portfolio />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 import "../../index.css";
+
 export default function Navbar() {
   return (
-   <div>
-  <nav className="navbar navbar-expand-lg bg-body-tertiary w-full mb-0">
-    <div className="container-fluid ">
-    <h2>Start Bootstrap</h2>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="#">Portfolio</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-        
-          <li className="nav-item">
-            <a className="nav-link " >Contact</a>
-          </li>
-        </ul>
-     
-      </div>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary w-full mb-0">
+        <div className="container-fluid">
+          <h2>Start Bootstrap</h2>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to={'/'}>Portfolio</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/About'}>About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/Contact'}>Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </nav>
-</div>
-
-  )
+  );
 }
-
